edc37c6203e283fbfa7511e93d5455c81ef57aad
Jason Berney <jasonrberney@gmail.com>
2018-01-10 14:03:50 -0800

    Get dimensions of phone and define aspect ratio. Add styles, fit map to screen.

--- MapScreen.js
+++ MapScreen.js
@@ -1,22 +1,38 @@
 import React from 'react';
-import { StyleSheet, View, Button } from 'react-native';
+import { StyleSheet, View, Button, Dimensions, TouchableOpacity } from 'react-native';
 import MapView from 'react-native-maps'; // 0.19.0
 import "prop-types"; // Supported builtin module
 //import { MapView } from "expo";
 //import { StackNavigator } from 'react-navigation';
 // 1.0.0-beta.23
 
-export default class MapScreen extends React.Component {
+const { width, height } = Dimensions.get('window');
+
+const ASPECT_RATIO = width / height;
+const LATITUDE = 47.6062;
+const LONGITUDE = -122.3321;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
+class MapScreen extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      mapRegion: { latitude: LATITUDE,
+        longitude: LONGITUDE,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA, },
+      markers: [],
+    };
   }
   static navigationOptions = {
     title: 'Welcome',
   };
 
-  state = {
-    mapRegion: { latitude: 47.6062, longitude: -122.3321, latitudeDelta: 0.0922, longitudeDelta: 0.0421 }
-  };
+  // state = {
+  //   mapRegion: { latitude: 47.6062, longitude: -122.3321, latitudeDelta: 0.0922, longitudeDelta: 0.0421 }
+  // };
 
   _handleMapRegionChange = mapRegion => {
     this.setState({ mapRegion });
@@ -25,7 +41,7 @@ export default class MapScreen extends React.Component {
   render() {
     return (
       
-      <MapView style={{ height: 400, width: 400 }} region={this.state.mapRegion} onRegionChange={this._handleMapRegionChange}>
+      <MapView provider={this.props.provider} style={styles.map} region={this.state.mapRegion} onRegionChange={this._handleMapRegionChange}>
         <MapView.Marker key={1} coordinate={{latitude: 47.6062, longitude: -122.3321}}/>
       </MapView>
       
@@ -33,13 +49,40 @@ export default class MapScreen extends React.Component {
   }
 }
 
-// const styles = StyleSheet.create({
-//   map: {
-//     flex: 1,
-//     flexDirection: 'column',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//     width: 150,
-//     length: 150,
-//   },
-// });
+MapScreen.propTypes = {
+  provider: MapView.ProviderPropType,
+};
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  bubble: {
+    backgroundColor: 'rgba(255,255,255,0.7)',
+    paddingHorizontal: 18,
+    paddingVertical: 12,
+    borderRadius: 20,
+  },
+  latlng: {
+    width: 200,
+    alignItems: 'stretch',
+  },
+  button: {
+    width: 80,
+    paddingHorizontal: 12,
+    alignItems: 'center',
+    marginHorizontal: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginVertical: 20,
+    backgroundColor: 'transparent',
+  },
+});
+
+module.exports = MapScreen;

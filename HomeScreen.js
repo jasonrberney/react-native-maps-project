25e45e91d007d11d37135e86ca2fa6b42526d079
U-INTEGRALGIS\msaiget <msaiget@mark-PC.integralgis.local>
2018-01-23 16:32:52 -0800

    Move getCapitals call to HomeScreen mount.

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,33 +1,53 @@
 import React from 'react';
 import { View, Button, StyleSheet } from 'react-native';
+import { getCapitals } from './helpers/dataLoader'
+import {addCapital} from './redux/reducers.js'
+import { dispatch, bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 //import { StackNavigator } from 'react-navigation';
- // 1.0.0-beta.23
+// 1.0.0-beta.23
 
-export default class HomeScreen extends React.Component {
+class HomeScreen extends React.Component {
   constructor(props) {
     super(props);
   }
   static navigationOptions = {
     title: 'Home Screen',
   };
+
+  componentDidMount() {
+    let test = getCapitals()
+    .then((data) => {
+      data.map(capital => (this.props.dispatch(addCapital(capital))))
+    })
+    .catch((error) => {console.log(error)}); // .bind(this) would go in between curly and paren if it wasn't arrow function
+  }
+
   render() {
     const { navigate } = this.props.navigation;
     return (
       <View>
-        <Button
-          title="Find some capitals"
-          onPress={() =>
-            navigate('Map')
-          }
-        />
-        <Button
-          title="List some capitals"
-          onPress={() =>
-            navigate('List')
-          }
-        />
+      <Button
+      title="Find some capitals"
+      onPress={() =>
+        navigate('Map')
+      }
+      />
+      <Button
+      title="List some capitals"
+      onPress={() =>
+        navigate('List')
+      }
+      />
       </View>
     );
   }
 }
 
+function mapStateToProps(state) {
+    return {
+        data: state
+    }
+}
+
+export default connect(mapStateToProps)(HomeScreen)
